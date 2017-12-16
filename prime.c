5221bec6637610f2d0eece49fa9d358cb98b6ea4
Hazwan <mohdhazwanmdesa@gmail.com>
2017-12-16 11:30:59 +0800

    prime.c: getprime function created
    pass and recieve value between function
    
    Signed-off-by: Hazwan <mohdhazwanmdesa@gmail.com>

--- prime.c
+++ prime.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 #include<stdlib.h>
 
+
+
+int getprime (int nthprime)
+{
+	int val=100;
+
+	printf("nthprime\n");
+	return val;
+
+}
+
+
 int main(int argc, char *argv[])
 {
+	int result;
+	int nthprime;
+	nthprime=100;
+	result = getprime(nthprime);
+	printf("Result=%d\n",result);
 	printf("prime\n");
 	return 0;
 
